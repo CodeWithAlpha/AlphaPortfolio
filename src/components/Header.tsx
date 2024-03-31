6a9ff48f2cfaa749b6088c76b5c398e42f9dd860
FrontDevMain <sharad.kumar@tramo.in>
2024-03-31 18:57:21 +0530

    update hamburger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,24 +3,88 @@ import CustomButton from "./button/CustomButton";
 import { useSettingContext } from "../theme/SettingContext";
 import { LightMode } from "../assets/Icons/Lightmode";
 import { Darkmode } from "../assets/Icons/Darkmode";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import useWindowDimensions from "./hooks/useWindowDiamensions";
 import { Hamburger } from "../assets/Icons/hanburger";
+import { CloseIcon } from "../assets/Icons/CloseIcon";
 
 function Header({ backgroundColor, scrollTo }: any) {
   const { themeMode, onToggleMode } = useSettingContext();
   const [active, setActive] = useState("home");
+  const [open, setOpen] = useState(false);
+
+  const navbar = [
+    { name: "Home", id: "home" },
+    { name: "About", id: "about" },
+    { name: "Skill", id: "skill" },
+    { name: "Project", id: "project" },
+    { name: "Contact", id: "contact" },
+  ];
 
   const { height, width } = useWindowDimensions();
 
   if (width < 800) {
     return (
-      <Stack sx={{ backgroundColor: backgroundColor }}>
-        <Stack height={{ xs: 40, md: 80 }} alignItems={"end"}>
-          <Hamburger />
+      <>
+        <Stack sx={{ backgroundColor: backgroundColor }}>
+          <Stack height={{ xs: 40, md: 80 }} alignItems={"end"}>
+            <Hamburger onClick={() => setOpen(!open)} />
+          </Stack>
         </Stack>
-      </Stack>
+        <AnimatePresence>
+          {open && (
+            <Stack
+              component={motion.div}
+              sx={{
+                height: "100vh",
+                width: "80%",
+                bgcolor: "#f6f5f1",
+                borderLeft: "1px solid #e0e0e0",
+                position: "absolute",
+                top: 0,
+                right: 0,
+                zIndex: 1000,
+              }}
+              initial={{ width: 0 }}
+              animate={{ width: "80%" }}
+              exit={{ width: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <CloseIcon onClick={() => setOpen(!open)} />
+              <Stack
+                alignItems={"center"}
+                justifyContent={"center"}
+                gap={1}
+                sx={{ overflow: "clip" }}
+              >
+                {navbar.map((item, index) => {
+                  return (
+                    <Typography
+                      color={
+                        active == item.id ? "primary.main" : "text.secondary"
+                      }
+                      variant={active == item.id ? "h6" : "subtitle1"}
+                      onClick={() => {
+                        scrollTo(item.id);
+                        setActive(item.id);
+                        setOpen(!open);
+                      }}
+                      sx={{ cursor: "pointer" }}
+                      component={motion.div}
+                      initial={{ x: "100vw" }}
+                      animate={{ x: 0 }}
+                      transition={{ duration: 0.5, delay: index / 7 + 0.1 }}
+                    >
+                      {item.name}
+                    </Typography>
+                  );
+                })}
+              </Stack>
+            </Stack>
+          )}
+        </AnimatePresence>
+      </>
     );
   }
 
